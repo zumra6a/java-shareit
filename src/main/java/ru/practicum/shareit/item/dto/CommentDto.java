9ba723a105738ee4a4bb961af1ee29c22793b079
Шухрат Шадмонов <zumra6a@yandex.ru>
2024-03-31 11:53:37 +0300

    fix: add string max size in dto validators

--- src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.item.dto;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 import ru.practicum.shareit.validation.Marker;
@@ -8,5 +9,6 @@ import ru.practicum.shareit.validation.Marker;
 @Data
 public class CommentDto {
     @NotBlank(groups = {Marker.OnCreate.class})
+    @Size(max = 255)
     private String text;
 }
--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.user.dto;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.Builder;
 import lombok.Data;
@@ -13,9 +14,11 @@ public class UserDto {
     private Long id;
 
     @NotBlank(groups = {Marker.OnCreate.class})
+    @Size(max = 255)
     private String name;
 
     @Email(groups = {Marker.OnCreate.class, Marker.OnUpdate.class})
     @NotBlank(groups = {Marker.OnCreate.class})
+    @Size(max = 512)
     private String email;
 }
